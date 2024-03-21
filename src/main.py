1cec44d3ec09c4b428db6f0e4170bab50d2a272c
KarlVerdad <karlfizz1999@gmail.com>
2024-03-21 17:42:54 +0800

    v0.05a: Data Gathering + Paths of the files to test are no longer hard coded 	> main.py processes both files and directories + Added parsed arguments 	> "path" - Path to file OR directory to test 	> [-s] - Toggle to save the results + Results are appended to "preliminary_results.txt"

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
-import random
 import os
+import argparse
+import random
 from modules.GraphAP import GraphAP
 from colorama import Fore
+from datetime import datetime
 
-random.seed(6378912)
 
-FILE_TO_TEST = "assign200.txt"
-DATA_RELATIVE_DIR = "../test"	
-# 0 => no unknowns, 1 => all unknown
-DELTA_OPTIONS = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+SEED = 6378912
+RESULTS_FILE = "../preliminary_results.txt"		# Relative path
+VALID_EXT = ('.txt', '.mama')		# Valid input file extensions
+DELTA_OPTIONS = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]		# 0 => no unknowns, 1 => all unknown
+
+random.seed(SEED)
 
 
 ## Performs semi-online matching on a graph
@@ -40,7 +43,10 @@ def semionline(graphAP: GraphAP, delta):
 
 ## Prototype function for semionline matchihng
 ## Input: absolute path to input 'file_path'
+## Output: Dictionary of {delta: empirical c. ratio}
 def simulate_semionline(file_path):
+	file_name = os.path.basename(file_path)
+	print(f"===File: {file_name}===")
 	G = GraphAP(file_path)
 
 	# Offline matching (Karp)
@@ -70,15 +76,79 @@ def simulate_semionline(file_path):
 		print(empirical_competitive_ratio)
 
 	# Display summarized results
-	print("==Summary==")
-	print({d: float(f"{c:.4f}") for d, c in competitive_ratio_results.items()})
+	print(f"===Summary: {file_name}===")
+	summarized_results = {d: round(c, 3) for d, c in competitive_ratio_results.items()}
+	print("Delta\tEmpirical C. Ratio")
+	for d, c in summarized_results.items():
+		print(f"{d}\t{c}")
+
+	return competitive_ratio_results
 	
 
-if __name__ == "__main__":
-	# Get absolute path of input data
+## Converts a relative (to this file) path to an absolute path
+## Input: directory containing file 'rel_dir', raw 'file_name'
+## Output: absolute path to a single file
+def rel2abs_path(rel_dir, file_name):
 	dir = os.path.dirname(__file__)
-	rel_path = os.path.join(dir, DATA_RELATIVE_DIR, FILE_TO_TEST)
-	file_path = os.path.abspath(rel_path)	
+	rel_path = os.path.join(dir, rel_dir, file_name)
+	return os.path.abspath(rel_path)
 
-	# Perform simulations
-	result = simulate_semionline(file_path)
+
+## Appends results in the RESULTS_FILE
+def store_result(file_path, result):
+	# Stored data: File name, date, seed, n, results
+	with open(file_path, "r") as f1:
+		n = int(f1.readline())
+	file_name = os.path.basename(file_path)
+	time = datetime.now().strftime("%d %B %Y, %H:%M:%S")
+	header = f"> {file_name} | {time} | Seed={SEED} | n={n}\n"
+
+	with open(f"{rel2abs_path('.', RESULTS_FILE)}", "a") as f2:
+		# Stores header
+		f2.write(header)
+		
+		# Stores results line by line
+		for d, c in result.items():
+			entry = f"{d}\t\t{c}\n"
+			f2.write(entry)
+
+
+if __name__ == "__main__":
+	# Parameters
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("path", help="Directory/File to use as input")
+	parser.add_argument("-s", "--save", action='store_true',
+            help="Toggle to append the result(s) in the results file")
+
+	args = parser.parse_args()
+
+	# Different behaviour based on if the path is a directory or file
+	path = os.path.abspath(args.path)
+	print(path)
+	if os.path.isfile(path):
+		# Path argument is a file
+		result = simulate_semionline(path)
+
+		# Stores results
+		if args.save:
+			store_result(path, result)
+
+	elif os.path.isdir(path):
+		# Path argument is a directory
+		for file in sorted(os.listdir(path)):
+			if file.endswith(VALID_EXT):
+				file_path = os.path.join(path, file)
+				result = simulate_semionline(file_path)
+				print("")
+
+				# Store results
+				if args.save:
+					store_result(file_path, result)
+
+	else:
+		raise Exception(f"{Fore.RED}Invalid path argument!{Fore.WHITE}")
+
+	if args.save:
+			print(f"Results saved in {rel2abs_path('.', RESULTS_FILE)}")
+	
