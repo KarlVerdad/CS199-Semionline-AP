1ad8a07e138d91b72d7b9d17780984fb9ebfe963
KarlVerdad <karlfizz1999@gmail.com>
2024-05-17 09:57:39 +0800

    v0.12: Data gathering + Ran semionline algorithm on 30 different seeds 	> Raw and compiled results saved in results/semionline + Ran semionlineML algorithm on 30 different seeds 	> Raw and compiled results saved in results/semionline

--- src/main.py
+++ src/main.py
@@ -14,9 +14,9 @@ VALID_EXT = ('.txt')		# Valid input file extensions
 
 EPSILON_OPTIONS = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
 K_OPTIONS = [10, 30, 50]
-DELTA_OPTIONS = [0, 0.25, 0.5, 0.75, 1]
+# DELTA_OPTIONS = [0, 0.25, 0.5, 0.75, 1]
 # DELTA_OPTIONS = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]		# 0 => no unknowns, 1 => all unknown (δ - proportion of adversarial)
-# DELTA_OPTIONS = [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
+DELTA_OPTIONS = [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
 
 #region =====OnlineML=====
